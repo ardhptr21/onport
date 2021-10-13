ff8a6b551e50085d5941d48fa3e5d318bbb13f12
= <ardhiputrapradana21@gmail.com>
2021-10-13 21:14:28 +0700

    frontend: setup colors & font family

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "encode-sans": ['"Encode Sans"', "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: "#101920",
+        secondary: "#888888",
+      },
+    },
   },
   variants: {
     extend: {},
