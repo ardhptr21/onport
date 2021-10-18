b54fbdc440c4fa15acd0634252facf5561640b94
= <ardhiputrapradana21@gmail.com>
2021-10-18 18:48:01 +0700

    backend: create controller for get all profile and create parseError function profile

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,5 +1,42 @@
+const { isValidObjectId } = require("mongoose");
+const Profile = require("../models/Profile");
+
+const parseError = ({ message, errors }) => {
+  const parse = {};
+
+  if (errors != null) {
+    Object.keys(errors).forEach((key) => {
+      const error = errors[key];
+      parse[key] = error.properties.message;
+    });
+  }
+
+  return parse;
+};
+
 module.exports.getAll = async (req, res) => {
-  res.send("GET ALL PROFILES");
+  // get userId for the params id
+  const _userId = req.params.id;
+
+  // check if _userId is not valid object id
+  if (!isValidObjectId(_userId))
+    return res.status(400).json({ status: 400, success: false, message: `Id ${_userId} is not valid object id` });
+
+  try {
+    const profile = await Profile.findOne({ _userId: _userId });
+
+    // check if profile is null
+    if (!profile)
+      return res
+        .status(404)
+        .json({ status: 404, success: false, message: `Profile with user id ${_userId} is not found` });
+
+    res.status(200).json({ status: 200, success: true, data: profile });
+  } catch (err) {
+    const error = parseError(err);
+    res.status(500).json({ status: 500, success: false, error });
+    console.log(err);
+  }
 };
 
 /**----------------------
