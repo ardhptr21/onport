d9d287f264076e32c0ac6e62ffc915f188d72b6a
= <ardhiputrapradana21@gmail.com>
2021-10-19 15:03:00 +0700

    backend: create function controller to get only projects profile

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -136,7 +136,27 @@ module.exports.updateSkill = async (req, res) => {
  *    PROJECTS CONTROLLER
  *------------------------**/
 module.exports.getProjects = async (req, res) => {
-  res.send("GET ALL PROJECTS");
+  // get user id from the params id value
+  const _userId = req.params.id;
+
+  // check if _userId is not valid id
+  if (!isValidObjectId(_userId))
+    return res.status(400).json({ status: 400, success: false, message: `Id ${_userId} is not valid object id` });
+
+  try {
+    const profile = await Profile.findOne({ _userId });
+
+    // check if profile is null
+    if (!profile)
+      return res
+        .status(404)
+        .json({ status: 404, success: false, message: `Profile projects with user id ${_userId} is not found` });
+
+    res.status(200).json({ status: 200, success: true, data: profile.projects });
+  } catch (err) {
+    res.status(500).json({ status: 500, success: false, message: err.message });
+    console.log(err);
+  }
 };
 module.exports.addProject = async (req, res) => {
   res.send("ADD NEW PROJECT");
