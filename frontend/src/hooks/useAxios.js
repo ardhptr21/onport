40fe96c46563b722172f99bedd0ca375f47b8814
= <ardhiputrapradana21@gmail.com>
2021-10-19 18:02:37 +0700

    backend: create useAxios hooks (axios instance)

---
+++ frontend/src/hooks/useAxios.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const useAxios = () => {
+  const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:8080",
+    timeout: 3000,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return axiosInstance;
+};
+
+export default useAxios;
